332d20187c17080d253f56828e7685bedf219897
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2015-06-11 16:54:26 -0400

    Initial Commit

---
+++ Geometry-2nd/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Geometry-2nd
+//
+//  Created by C4Q on 6/11/15.
+//  Copyright (c) 2015 C4Q. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
